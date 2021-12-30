8cbd9a851f5cd5e66b0e4dedeb8c721243a64340
Pietvc <piet.vancleemput@gmail.com>
2021-12-30 18:22:23 +0100

    Dit werkt!

---
+++ zigbee2mqtt-data/81464.js
@@ -0,0 +1,25 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    zigbeeModel: ['PROLIGHT E27 WARM WHITE'],
+    model: '81464',
+    vendor: 'Prolight',
+    description: 'My super lamp!',
+    // Note that fromZigbee, toZigbee and exposes are missing here since we use extend here.
+    // Extend contains a default set of fromZigbee/toZigbee converters and expose for common device types.
+    // The following extends are available:
+    // - extend.switch
+    // - extend.light_onoff_brightness
+    // - extend.light_onoff_brightness_colortemp
+    // - extend.light_onoff_brightness_color
+    // - extend.light_onoff_brightness_colortemp_color
+    extend: extend.light_onoff_brightness(),
+};
+
+module.exports = definition;
